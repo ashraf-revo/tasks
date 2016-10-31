660c3be8c3d838ec8b449fb4e1a45164e1289232
ashraf <ashraf1abdelrasool@gmail.com>
2016-10-31 21:20:11 +0200

    done untile

--- src/main/java/Q5/Q5.java
+++ src/main/java/Q5/Q5.java
@@ -6,18 +6,18 @@ import static jdk.nashorn.internal.objects.Global.print;
  * Created by ashraf on 31/10/16.
  */
 public class Q5 {
-    public  static int scrollDown (Integer a,String v)                  // void   Integer       v
+    public  static int scrollDown (Integer a,String v)
     {
-        return (a == 0) ? scrollDown(100,v) : scrollDown(a-1,v);            // = not ==)    return   v
+        return (a == 0) ? scrollDown(100,v) : scrollDown(a-1,v);
     }
 
-   public static void main(int arg1, int arg2)       //public static void    int arg2
+   public static void main(int arg1, int arg2)
     {
         if (arg2 <= 0)
             if (arg2 < 0)
-                print("Too small.") ;                 //;
+                print("Too small.") ;
             else
-                print("Just right.");                //;
+                print("Just right.");
         scrollDown(arg1, "start");
     }
 
